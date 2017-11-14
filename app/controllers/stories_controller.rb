8f001ec31d27b762d753a2291d3b690d12437450
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-14 16:26:26 -0500

    Unchecked categories are now deleted from database

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -15,6 +15,7 @@ class StoriesController < ApplicationController
 
   def update
     if @story.update(story_params)
+      update_tags(@story)
       redirect_to story_path(@story)
     else
       render :new
@@ -30,14 +31,7 @@ class StoriesController < ApplicationController
     @story = Story.new(story_params)
     @story.user = current_user
     if @story.save
-      params[:story][:categories].each do |category_id|
-        unless category_id == "" || Category.find(category_id).nil?
-          @tag = StoryCategory.new
-          @tag.category = Category.find(category_id)
-          @tag.story = @story
-          @tag.save
-        end
-      end
+      create_tags(@story)
       redirect_to story_path(@story)
     else
       render :new
@@ -50,6 +44,29 @@ class StoriesController < ApplicationController
   end
 
   private
+
+  def create_tags(story)
+    params[:story][:categories].each do |category_id|
+      unless category_id == "" || Category.find(category_id).nil? || StoryCategory.find_by(story: story, category: Category.find(category_id))
+        @tag = StoryCategory.new
+        @tag.category = Category.find(category_id)
+        @tag.story = story
+        @tag.save
+      end
+    end
+  end
+
+  def update_tags(story)
+    @categories = story.categories
+    @categories.each do |category|
+      unless params[:story][:categories].include?(category.id.to_s)
+        @tag = StoryCategory.find_by(story: story, category: category)
+        @tag.destroy
+      end
+    end
+
+    create_tags(story)
+  end
 
   def set_story
     @story = Story.find(params[:id])
