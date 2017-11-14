320647aced3917cb2dd6cbab5fd8ab05dc7ca460
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-14 11:41:02 -0500

    Added story validations

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,8 @@ class Story < ApplicationRecord
   belongs_to :user
   has_many :story_categories, dependent: :destroy
   has_many :categories, trough: :story_category
+
+  validates :title, presence: true, uniqueness: true, length: { min: 3, max: 40 }
+  validates :description, presence: true, length: { min: 40, max: 280 }
+  validates :categories, uniqueness: true
 end
