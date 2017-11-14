8b1cc9015a31814bef24cd6161b57bdf46d00731
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-14 11:32:58 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :stories, dependent: :destroy
+
+  validates :nickname, presence: true, uniqueness: true, length: { max: 20 }
+  validates :email, presence: true, uniqueness: true
+  validates :bio, length: { max: 512 }
 end
