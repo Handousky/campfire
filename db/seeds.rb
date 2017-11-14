71da27e34bdcfd9a73f5d483a0a51e9024148e9b
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-14 11:16:12 -0500

    Added seed for the categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seeding all the Categories in the DB
+categories = [
+  "Based in real facts",
+  "Fanfiction",
+  "Gore",
+  "Killer",
+  "Mistery",
+  "Monsters",
+  "Psychological",
+  "Realistic",
+  "Religion",
+  "Screamer",
+  "Spirits"
+]
+
+puts "Seeding Categories"
+categories.each { |category| Category.create(name: category) }
+puts "Categories seeded"
