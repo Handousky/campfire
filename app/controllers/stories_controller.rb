0b20f8f1ae70d1d33e5e6a69283f3b16a2a2f1ec
Robin Handous <robin.handous@gmail.com>
2017-11-14 11:57:03 -0500

    create simple controller for stories

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,50 @@
+class StoriesController < ApplicationController
+  before_action :set_story, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @stories = Story.all
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to story_path(@story)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to story_path(@story)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @story.destroy
+    redirect_to stories_path
+  end
+
+  private
+
+  def set_story
+    @story = Story.find(params[:id])
+  end
+
+  def story_params
+    params.require(:story).permit(:title, :description)
+  end
+end
