0c165c37cf42510ef16bfe68b977993d39be9db3
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-17 11:01:37 -0500

    Added Ratings table/model. Added star image assets and js to handle dynamic ratings. Added proper dependencies

---
+++ app/models/rating.rb
@@ -0,0 +1,4 @@
+class Rating < ApplicationRecord
+  belongs_to :story
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117145929) do
+ActiveRecord::Schema.define(version: 20171117155557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 20171117145929) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "story_id"
+    t.integer  "user_id"
+    t.integer  "score",      default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["story_id"], name: "index_ratings_on_story_id", using: :btree
+    t.index ["user_id"], name: "index_ratings_on_user_id", using: :btree
   end
 
   create_table "slides", force: :cascade do |t|
@@ -85,6 +95,8 @@ ActiveRecord::Schema.define(version: 20171117145929) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "ratings", "stories"
+  add_foreign_key "ratings", "users"
   add_foreign_key "slides", "stories"
   add_foreign_key "stories", "users"
   add_foreign_key "story_categories", "categories"
