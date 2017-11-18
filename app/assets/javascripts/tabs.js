a284e0238f6e2e2e93844b06112fce7163dc340a
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-17 21:29:46 -0500

    Fixed tab bug

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -8,6 +8,6 @@ function openTab(evt, tabName) {
   for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(tabName).style.display = "block";
+  document.getElementById(tabName).style.display = "flex";
   evt.currentTarget.className += " active";
 }
