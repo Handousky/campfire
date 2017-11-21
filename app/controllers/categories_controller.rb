f19ebf98b2b200d3e4ad1165ff633139fd584d13
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-21 11:59:32 -0500

    Added category navigation (show category page)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,15 @@
 class CategoriesController < ApplicationController
+  before_action :render_footers
+  skip_before_action :authenticate_user!
+
   def show
-    @stories = Category.find(params[:id]).stories
+    @category = Category.find(params[:id])
+    @stories = @category.stories
+    if user_signed_in?
+    @stories.each do |story|
+      Rating.create(story: story, user: current_user, score: 0) unless Rating.find_by(story: story, user: current_user)
+    end
+    @ratings = current_user.ratings.where(story: @stories)
+  end
   end
 end
