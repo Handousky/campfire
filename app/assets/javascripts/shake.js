5d321308b2a50bb6fae023b364dbf6fc5b387704
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-24 14:48:47 -0500

    Fixed weird behavior of the two arrows on the home page

--- app/assets/javascripts/shake.js
+++ app/assets/javascripts/shake.js
@@ -1,5 +1,15 @@
-function shakeThrough() {
-  var breakthrough = document.getElementById("breakThrough");
+function shakeThrough1() {
+  var breakthrough = document.getElementById("breakThrough1");
+  //Restart breakthrough animaton
+
+  var newBreakthrough = breakthrough.cloneNode(true);
+  newBreakthrough.classList.add("shake");
+
+  breakthrough.parentNode.replaceChild(newBreakthrough, breakthrough);
+}
+
+function shakeThrough2() {
+  var breakthrough = document.getElementById("breakThrough2");
   //Restart breakthrough animaton
 
   var newBreakthrough = breakthrough.cloneNode(true);
